81d4e363338ffae3925ff02e04e8c65bdc6b7ff5
malithw1987465 <maliith.20223218@iit.ac.lk>
2023-10-12 19:43:41 +0530

    updated db

--- assignments/JS/In Memory POS/db/db.js
+++ assignments/JS/In Memory POS/db/db.js
@@ -1,21 +1,16 @@
 //you can store data like as follows
-var customerDB = [
-    {id: "C00-001", name: "Sachin Thamalsha", address: "Mathara", salary: 100000},
-    {id: "C00-002", name: "Ranjith Perera", address: "Panadura", salary: 200000},
-    {id: "C00-003", name: "Kavindu Perera", address: "Panadura", salary: 300000}
+export var customerDetail = [
+    {id: 'C00-001', name:'Malith Jayasundara', address: 'Hambanthota', salary: 50000},
+    {id: 'C00-002', name:'Rashmi Dias', address: 'Matara', salary: 25000},
+    {id: 'C00-003', name:'Hirusha Navod', address: 'Galle', salary: 65000}
 ];
 
-var itemDB = [
-    {code:"I00-001",description:"Lux",qtyOnHand: 100,unitPrice: 145.00},
-    {code:"I00-002",description:"Sunlight",qtyOnHand: 150,unitPrice: 345.00},
-    {code:"I00-003",description:"Light Boy",qtyOnHand: 400,unitPrice: 245.00}
+export var itemDetail = [
+    {code: 'I00-001', name:'Surf Excel 1kg', price: 550.00, quantity: 200},
+    {code: 'I00-002', name:'Rice Basmathi', price: 95.00, quantity: 100},
+    {code: 'I00-003', name:'Milk Powder',price: 1150.00, quantity: 122}
 ];
 
-var orderDB = [
-    {oid:"OID-001", date:"2023/10/06", customerID:"C00-001",
-        orderDetails:[
-            {oid:"OID-001", code:"I00-001", qty:10, unitPrice:145.00},
-            {oid:"OID-001", code:"I00-002", qty:2, unitPrice:345.00}
-        ]
-    }
-];
+export var orders = [];
+
+export var orderDetail = [];
